e1d53e8faa9c655915617fdf65ee6a01b9f02681
m-gunn <100558621+Uni-mGunn@users.noreply.github.com>
2022-02-28 20:08:18 +1030

    Week0

---
+++ function-1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void hello(){
+
+    std::cout << "Object-Oriented Programming";
+
+    // std is a namespace. Think about it as a declarative region inside which things are defined. 
+    // std::cout << "abc \n"; this is equivalent to print("abc") with \n for a new line.
+    // cout is defined in the namespace std   
+    
+    // use cout to print "Object-Oriented Programming".  
+
+}
