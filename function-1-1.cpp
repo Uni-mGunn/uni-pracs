7b0fad326e8ddd84b91649e390b843e3fe1ef12d
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-03-02 15:56:24 +1030

    week1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int sum_array(int array[], int n) {
-    unsigned int sum = 0;
     if (n<1) {
-        return sum;
+        return 0;
     }
+    unsigned int sum = 0;
     for(int i = 0; i < n; i++) {
         sum += array[i];
     }
--- function-3-3.cpp
+++ function-3-3.cpp
@@ -4,9 +4,9 @@ double weightedaverage(int array[], int n) {
     if (n<=0 && n%2 == 0) {
         return 0.0;
     } 
-    double weightAve = 0;
+    double weightAve = 0.0;
     for(int i = 0; i < n; i++) {
-        int sum = 0;
+        double sum = 0;
         for(int j = 0; j < n; j++) {
             if (array[i] == array[j]){
                 sum++;
