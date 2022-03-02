52b3d7de4cffe55212e3cb3960b9d9825bb478df
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-03-02 15:45:24 +1030

    week1

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -5,17 +5,20 @@ bool fanarray(int array[], int n) {
     if (n<=0) {
         return false;
     } 
-    int order = array[0];
-    for(int i = 1; i < n; i++) {
-        if (i < n/2){
-            if (array[i] >= order){
-                order = array[i];
+    int prev = array[0];
+    for(int i = 1; i < n; i++) { // n = 4
+    // n/2 == 2
+    // 0, 1 wants bigger
+    // 2, 3 wants smaller
+        if (i*2 < n){ // wants to be be bigger than prev
+            if (array[i] >= prev){
+                prev = array[i];
             } else {
                 return false;
             }
-        } else {
-            if (array[i] <= order) {
-                order = array[i];
+        } else { // wants to be be smaller than prev
+            if (array[i] <= prev) {
+                prev = array[i];
             } else {
                 return false;
             }
