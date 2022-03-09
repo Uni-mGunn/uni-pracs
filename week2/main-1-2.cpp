7ea230df48b4f1aaed943b2fc46ad9218b4edc5d
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-03-09 16:12:47 +1030

    .

--- week2/main-1-2.cpp
+++ week2/main-1-2.cpp
@@ -2,10 +2,10 @@
 
 // The `extern` keyword tells the compiler that the count function exists,
 // but will be implemented somewhere else
-extern int average(int[], int);
+extern int identity(int array[10][10]);
 
 int main() {
-    int array[5] = {4,5,6,7,8};
-    std::cout << "The number is: " << average(array, 5) << std::endl;
+    int array[10][10] = {{4,5,6,7,8,4,5,6,7,8},{4,5,6,7,8,4,5,6,7,8},{4,5,6,7,8,4,5,6,7,8},{4,5,6,7,8,4,5,6,7,8},{4,5,6,7,8,4,5,6,7,8},{4,5,6,7,8,4,5,6,7,8},{4,5,6,7,8,4,5,6,7,8},{4,5,6,7,8,4,5,6,7,8},{4,5,6,7,8,4,5,6,7,8},{4,5,6,7,8,4,5,6,7,8}};
+    std::cout << "The number is: " << identity(array) << std::endl;
     return 0;
 }
