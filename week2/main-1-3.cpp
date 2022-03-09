8a080b1006c54429b71cb7f18aab39d61d439074
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-03-09 16:14:45 +1030

    .

--- week2/main-1-3.cpp
+++ week2/main-1-3.cpp
@@ -4,20 +4,7 @@ using namespace std;
 // The `extern` keyword tells the compiler that the count function exists,
 // but will be implemented somewhere else
 
-void count_numbers(int array[4][4]) {
-    int sums[10] = {0,0,0,0,0,0,0,0,0,0};
-
-    for(int i = 0; i < 4; i++) {
-       for(int j = 0; j < 4; j++) {
-            sums[ array[i][j] ] += 1;
-        }
-    }
-
-    for(int i = 0; i < 10; i++) {
-        cout << to_string(i) + ":" + to_string(sums[i]) + ";";
-    }
-    cout << ";\n";
-}
+extern void count_numbers(int array[4][4]);
 
 int main() {
     int array[4][4] = { {4,5,6,7},{4,5,6,7},{4,5,6,7},{4,5,6,7} };
