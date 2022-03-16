1f4cecb5f923a457ec6d6d65656f582a60784192
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-03-16 15:17:29 +1030

    .

---
+++ function-2-2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int binary_to_number(int binary_digits[], int number_of_digits) {
+    int pow = 1;
+    int sum = 0;
+    for(int i = number_of_digits-1; i >= 0; i--){
+        sum = pow * binary_digits[i];
+        pow *= 2;
+    }
+    return sum;
+}
