e5117c78e38747669784783313c7eb65ceb78101
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-03-16 16:07:42 +1030

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -7,7 +7,7 @@
 void printer(int array[10][10]) {
     for(int i = 0; i < 10; i ++){
         for(int j = 0; j < 10; j++){
-            if(j == 10) {
+            if(j == 9) {
                 std::cout << array[i][j];
                 std::cout << "\n";
             } else {
