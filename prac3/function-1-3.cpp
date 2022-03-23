707fa88091ff33fe6dc960091bff690781410da3
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-03-23 13:37:27 +1030

    .

---
+++ prac3/function-1-3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void cpyia(int old_array[],int new_array[],int length){
+    int *p = new_array;
+    p = old_array;
+}
---
+++ prac3/function-1-4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void cpyda(double *old_array,double *new_array,int length){
+    int *p = *old_array;
+    new_array = p;
+}
