39764b59307d4786f50a490ec402a216e308629f
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-03-23 15:40:38 +1030

    Update function-2-2.cpp

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -8,7 +8,10 @@ int maximum_sum(int *nums,int length){
     for(int i = 0; i < length; i++){
         int sum = 0;
         for (int j = i; j < length; j++){
-            
+            sum += nums[j];
+            if(sum > max){
+                max = sum
+            }
         }
     }
     return max;
