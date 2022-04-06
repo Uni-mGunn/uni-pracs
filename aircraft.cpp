2f00aa05740ea255c26ee5db0555088d4654505d
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-04-06 16:30:23 +0930

    .

---
+++ aircraft.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "aircraft.h"
+#include "person.h"
+
+using namespace std;
+
+aircraft::aircraft(string _callsign,person _thePilot,person _theCoPilot){
+    callsign = _callsign;
+    thePilot = _thePilot;
+    theCoPilot = _theCoPilot;
+}
+
+void aircraft::setPilot(person _thePilot){
+    thePilot = _thePilot;
+}
+
+person aircraft::getPilot(){
+    return thePilot;
+}
+
+void aircraft::setCoPilot(person _theCoPilot){
+    theCoPilot = _theCoPilot;
+}
+
+person aircraft::getCoPilot(){
+    return theCoPilot;
+}
+
+void aircraft::printDetails(){
+    cout << callsign << endl << thePilot << endl << theCoPilot << endl;
+}
+
+
